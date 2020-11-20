015f86725fb9f1c19eedf97d9e4e0dee47a6349a
jonasrost <rojonas@student.chalmers.se>
2020-11-20 09:05:58 +0100

    fixed trans

--- src/Transportable.java
+++ src/Transportable.java
@@ -1,5 +1,5 @@
 
 /** An empty interface with the only function of being used as a separator between being transported and being able to transport
- * Classes that implement this is able to transport*/
+ * Classes that implement this is able to be transported*/
 public interface Transportable {
 }
